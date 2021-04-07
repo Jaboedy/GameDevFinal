1c7a948143fe71689a86f3b358397007106aaa12
Brody <tbunde01@gmail.com>
2021-04-07 17:48:36 -0400

    Update to current
    About to add collaborator

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,22 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
-    [SerializeField] int runSpeed = 5;
+    //config
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpHeight = 5f;
     private int nextSpawnID = 0;
 
+    //states
+    bool isAlive = true;
+    
+    //cached components
     Rigidbody2D myRigidBody;
+    Animator myAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        myAnimator.SetBool("Jumping", false);
         Run();
+        Jump();
         FlipSprite();
+        
     }
 
     private void Run()
@@ -27,6 +38,26 @@ public class Player : MonoBehaviour
         float xMove = Input.GetAxis("Horizontal") * runSpeed;
         Vector2 playerVelocity = new Vector2(xMove, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+
+        bool playerHasXSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHasXSpeed)
+        {
+            myAnimator.SetBool("Running", true);
+        }
+        else
+        {
+            myAnimator.SetBool("Running", false);
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Vector2 playerVelocity = new Vector2(myRigidBody.velocity.x, jumpHeight);
+            myRigidBody.velocity = playerVelocity;
+            myAnimator.SetBool("Jumping", true);
+        }
     }
 
     private void FlipSprite()
