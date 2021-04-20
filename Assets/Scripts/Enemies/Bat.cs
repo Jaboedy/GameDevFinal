8549d9fa0b3c38ea332153bc1014f9b899695340
Brody <tbunde01@gmail.com>
2021-04-20 09:44:27 -0400

    Add Bat Enemy Movement

--- Assets/Scripts/Enemies/Bat.cs
+++ Assets/Scripts/Enemies/Bat.cs
@@ -5,18 +5,32 @@ using UnityEngine;
 public class Bat : MonoBehaviour
 {
 
+    [SerializeField] float flySpeed;
+
     Player player;
     bool hasCollided = false;
+    bool isAlive = true;
+
+    Rigidbody2D myRigidbody;
+    Animator myAnimator;
+    BoxCollider2D myCollider;
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType<Player>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         hasCollided = false;
+        if (isAlive)
+        {
+            Fly();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -26,5 +40,62 @@ public class Bat : MonoBehaviour
             player.TakeDamage(1);
             hasCollided = true;
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Sword Attack"))
+        {
+            myAnimator.SetBool("Dying", true);
+            myCollider.enabled = false;
+            myRigidbody.velocity = new Vector2(0f, 0f);
+            isAlive = false;
+        }
+    }
+
+    private void Fly()
+    {
+        float xDirSign = Mathf.Sign(player.transform.position.x - transform.position.x);
+        Vector2 direction = new Vector2(player.transform.position.x - transform.position.x, player.transform.position.y - transform.position.y+0.6f);
+        float yDirSign = Mathf.Sign(direction.y);
+        LayerMask layers = LayerMask.GetMask("Ground", "Player");
+        Vector2 startPos = new Vector2(transform.position.x, transform.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(startPos, direction, Mathf.Infinity, layers);
+        if (hit.collider != null)
+        {
+            Debug.Log(hit.collider.gameObject.name);
+            Debug.DrawRay(startPos, direction);
+            if (hit.collider.gameObject.CompareTag("Player"))
+            {
+                myRigidbody.velocity = direction.normalized * flySpeed;
+            }
+            else
+            {
+                RaycastHit2D ceiling = Physics2D.Raycast(startPos, Vector2.up, Mathf.Infinity, layers);
+                RaycastHit2D floor = Physics2D.Raycast(startPos, Vector2.down, Mathf.Infinity, layers);
+                if (ceiling.distance < floor.distance)
+                {
+                    myRigidbody.velocity = new Vector2(0.7f * flySpeed * xDirSign, -0.7f * flySpeed);
+                }
+                else
+                {
+                    myRigidbody.velocity = new Vector2(0.7f * flySpeed * xDirSign, 0.7f * flySpeed);
+                }
+                    
+            }
+        }
+        
+        
+    }
+
+    public void DestroyAfterAnimation()
+    {
+        gameObject.SetActive(false);
+        Destroy(gameObject);
+    }
+
+    public void UnsetDying()
+    {
+        myAnimator.SetBool("Dying", false);
     }
 }
