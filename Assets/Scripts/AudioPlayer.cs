f2a9bbae5c6d911f3fb72d6a0a56d453e296061f
Brody <tbunde01@gmail.com>
2021-04-28 06:18:05 -0400

    Add some audio
    audio added for menu, normal room ambiance, player walking, and swinging sword

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        int audioPlayerCount = FindObjectsOfType<AudioPlayer>().Length;
+        if (audioPlayerCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Camera.main.transform.position;
+    }
+}
